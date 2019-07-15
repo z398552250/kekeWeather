7664911f08e45ea75fdcf56a61ee59f7e4c9688d
limeng <398552250@qq.com>
2019-07-15 15:39:03 +0800

    天气app 完成

--- app/src/main/java/com/keke/kekeweather/WeatherActivity.java
+++ app/src/main/java/com/keke/kekeweather/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.keke.kekeweather.gson.Forecast;
 import com.keke.kekeweather.gson.Weather;
+import com.keke.kekeweather.service.AutoUpdateService;
 import com.keke.kekeweather.util.HttpUtil;
 import com.keke.kekeweather.util.Utility;
 
@@ -174,7 +176,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
-
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
     }
 
